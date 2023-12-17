69eb7bd1d240a393695b5df05115698c0e4e6a2e
Rishab-Joshi16 <119615168+Rishab-Joshi16@users.noreply.github.com>
2023-12-17 22:37:24 +0530

    Add files via upload
    Updated repo with Leaky Bucket program also added along with Bellman Ford and Error corerction using CRC_CCITT(16_bits) programs

---
+++ CN_Lab_Programs/src/LeakyBucket.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class LeakyBucket {
+public static void main(String[] args) throws InterruptedException {
+int n,incoming,outgoing,store=0,bucketsize;
+Scanner scan=new Scanner(System.in);
+System.out.println("Enter bucket size, outgoing rate, number of inputs and incoming size");
+bucketsize = scan.nextInt();
+outgoing = scan.nextInt();
+n=scan.nextInt();
+incoming = scan.nextInt();
+while(n!=0){
+    System.out.println("Incoming size is " +incoming);
+    if(incoming<=(bucketsize-store)){
+        store+=incoming;
+        System.out.println("Bucket buffer size is " +store+ " out of " +bucketsize);
+
+    }
+    else{
+        System.out.println("PAcket loss: " +(incoming*(bucketsize-store)) );
+        store+=incoming;
+        System.out.println("Bucket buffer size is " +store+ " outgoing " +bucketsize);
+    }
+    store-=outgoing;
+    System.out.println("After outgoing: " +store+ " packets left out of " +bucketsize+ " in buffer");
+    n--;
+    Thread.sleep(3000);
+    scan.close();
+}
+    
+}
+}
